d89b2c568fddb036ea0acf956c31bee90c061c8c
NurMuhammadCSE <nurmuhammadcse@gmail.com>
2024-07-16 22:44:15 +0600

    Set Up Done

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,31 @@
+import { model, Schema } from "mongoose";
+import { TUser } from "./user.interface";
+
+const userSchema = new Schema<TUser>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  role: {
+    enum: ["admin", "user"],
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = model<TUser>("User", userSchema);
